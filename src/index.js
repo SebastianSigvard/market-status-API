bb6ce88c3b4bb551be319dfefe535f657ec4c0fe
Sebastian Sigvard <sebastian.sigvard@ptech.com.ar>
2022-05-22 09:59:21 -0300

    first version BittrexSocket

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ const BittrexSocket = require('./bittrex_socket_listener');
 
 const url = 'wss://socket-v3.bittrex.com/signalr';
 const hub = ['c3'];
-
 const apikey = '7379583862cc43d9b5a2b2ee550452d1';
 const apisecret = 'cc3bdafb64704d20a2413a78fea01be8';
 
+const OB_DEPTH = 25;
+
 const bittrexSocket = new BittrexSocket(url, hub, apikey, apisecret);
 
-const messageProcessor = (data) => {
+const messageProcessor = (data) => {// Dummy messageProcessor for testing
   console.log(data);
 };
 
 const channels = [
-  'heartbeat',
-  'trade_BTC-USD',
-  // 'orderbook_BTC-USD_25',
-  'balance',
+  'orderbook_BTC-USD_' + OB_DEPTH,
+  'orderbook_ETH-USD_' + OB_DEPTH,
 ];
 
 bittrexSocket.connect(messageProcessor).then( () => {
