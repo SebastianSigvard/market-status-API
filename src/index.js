8aab2a813f4a66c9bc9acc9129fd85685be47dfb
Sebastian Sigvard <sebastian.sigvard@ptech.com.ar>
2022-05-25 12:09:44 -0300

    websocket both endpoints implementation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-const express = require('express');
 const cors = require('cors');
+const sio = require('socket.io');
+const express = require('express');
 const bodyParser = require('body-parser');
 const MarketStatus = require('./market_status');
 
 const marketStatus = new MarketStatus();
 
+// API REST
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
 
 app.get('/tips/*', async (request, response) => {
   const cp = request.url.replace('/tips/', '');
@@ -32,4 +34,33 @@ app.post('/calculate-price', async (request, response) => {
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('App aviable on http://localhost:5000');
+});
+
+// WEWSocket
+
+const io = sio(process.env.PORT + 1 || 5001, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.on('tips', (currencyPair) => {
+    socket.emit('tips-response', marketStatus.processTipsReq(currencyPair));
+  });
+
+  socket.on('calculate-price',
+      (currencyPair, operation, amount, cap = undefined) => {
+        if ( !currencyPair || !operation || ! amount) {
+          socket.emit('calculate-price-response', {
+            status: 'error',
+            // eslint-disable-next-line max-len
+            message: 'body must include currencyPair operation and amount (cap optional)',
+          });
+          return;
+        }
+        socket.emit('calculate-price-response',
+            marketStatus.processCalPriReq(currencyPair, operation, amount, cap),
+        );
+      });
 });
